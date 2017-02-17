1549f7d7cac90b274d11f6d2d9c583bf48a7e8c9
dwpark1112 <dwpark@radnet.kr>
2017-02-17 10:09:41 +0900

    fix code style

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,74 +20,80 @@ public class Main {
 
 		Main main = new Main();
 		main.runPOI(file);
-//		main.runStreamingReader(file);
+		main.runStreamingReader(file);
 	}
 
-	public void runStreamingReader(File file) {
+	public void runStreamingReader( File file )
+	{
 		FileInputStream inputStream = null;
 		Workbook workbook = null;
 		try {
-			inputStream = new FileInputStream(file);
+			inputStream = new FileInputStream( file );
 			workbook = StreamingReader.builder()
-					.rowCacheSize(100)
-					.bufferSize(4096)
-					.open(inputStream);
+					.rowCacheSize( 100 )
+					.bufferSize( 4096 )
+					.open( inputStream );
 
-			for (Sheet sheet : workbook) {
-				System.out.println(sheet.getSheetName());
-				for (Row r : sheet) {
-					for (Cell c : r) {
-						System.out.println(c.getStringCellValue());
+			for ( Sheet sheet : workbook ) {
+				System.out.println( sheet.getSheetName() );
+				for ( Row r : sheet ) {
+					for ( Cell c : r ) {
+						System.out.println( c.getStringCellValue() );
 					}
 				}
 			}
-		} catch (FileNotFoundException e) {
+		}
+		catch ( FileNotFoundException e ) {
 		} finally {
-			IOUtils.closeQuietly(workbook);
-			IOUtils.closeQuietly(inputStream);
+			IOUtils.closeQuietly( workbook );
+			IOUtils.closeQuietly( inputStream );
 		}
 	}
 
-	public void runPOI(File file){
+	public void runPOI( File file )
+	{
 		FileInputStream inputStream = null;
 		Workbook workbook = null;
-		try {
-			inputStream = new FileInputStream(file);
 
-			workbook = new XSSFWorkbook(inputStream);
+		try {
+			inputStream = new FileInputStream( file );
+
+			workbook = new XSSFWorkbook( inputStream );
 			Iterator<Sheet> sheetIterator = workbook.sheetIterator();
-			while (sheetIterator.hasNext()) {
+			while ( sheetIterator.hasNext() ) {
 
 				Sheet firstSheet = sheetIterator.next();
 				Iterator<Row> iterator = firstSheet.rowIterator();
-				while (iterator.hasNext()) {
+				while ( iterator.hasNext() ) {
 					Row nextRow = iterator.next();
 
 					Iterator<Cell> cellIterator = nextRow.cellIterator();
-					while (cellIterator.hasNext()) {
+					while ( cellIterator.hasNext() ) {
 						Cell cell = cellIterator.next();
 
-						switch (cell.getCellTypeEnum()) {
-							case STRING :
-								System.out.println(cell.getStringCellValue());
-								break;
-							case NUMERIC :
-								System.out.println(cell.getNumericCellValue());
-								break;
-							case BLANK :
-								System.out.println(cell.getStringCellValue() + "(Blank)");
-								break;
-							default :
-								System.out.println("#####");
+						switch ( cell.getCellTypeEnum() ) {
+						case STRING:
+							System.out.println( cell.getStringCellValue() );
+							break;
+						case NUMERIC:
+							System.out.println( cell.getNumericCellValue() );
+							break;
+						case BLANK:
+							System.out.println( cell.getStringCellValue() + "(Blank)" );
+							break;
+						default:
+							System.out.println( "#####" );
 						}
 					}
 				}
 			}
-		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
+		}
+		catch ( FileNotFoundException e ) {
+		}
+		catch ( IOException e ) {
 		} finally {
-			IOUtils.closeQuietly(workbook);
-			IOUtils.closeQuietly(inputStream);
+			IOUtils.closeQuietly( workbook );
+			IOUtils.closeQuietly( inputStream );
 		}
 	}
 }
